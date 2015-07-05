7140394207e67fac40603823855616b97af19982
Avery <agrimsley@users.noreply.github.com>
2015-07-05 17:46:18 -0600

    nav arrows

--- app/helpers/episodes_helper.rb
+++ app/helpers/episodes_helper.rb
@@ -7,16 +7,16 @@ module EpisodesHelper
 
 		html << %Q{<span class="back">}
 		if back > 0
-			html << link_to("<", Episode.find(back), {:class => "back"})
+			html << link_to("", Episode.find(back), {:class => "back"})
 		else 
-			html << link_to("<", "#", {:class => "oldest"})
+			html << link_to("", "", {:class => "oldest"})
 		end
 		html << %Q{</span>}
 		html << %Q{<span class="forward">}
 		unless forward > Episode.last.id
-			html << link_to(">", Episode.find(forward), {:class => "forward"})
+			html << link_to("", Episode.find(forward), {:class => "forward"})
 		else
-			html << link_to(">", "#", {:class => "newest"})
+			html << link_to("", "", {:class => "newest"})
 		end
 		html << %Q{</span>}
 		return html.html_safe
